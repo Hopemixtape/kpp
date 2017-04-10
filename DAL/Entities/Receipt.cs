70be4fd8c5c241164308bdf7a8670eaca74adb59
hopemixtape <hopemixtape95@gmail.com>
2017-04-10 23:01:47 +0300

    first commit

---
+++ DAL/Entities/Receipt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Receipt
+    {
+        public int id { get; set; }
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public int ScoreNumber { get; set; }
+        public string ServiceName { get; set; }
+        public bool Privilege { get; set; }
+        public string SnstrumentReadings { get; set; }
+        public double SumPayment { get; set; }
+
+    }
+}
+//a.заголовок сторінки;
+//b.дата оплати;
+//c.прізвище та ініціали платника;
+//d.адреса платника;
+//e.номер особового рахунку;
+//f.найменування послуги;
+//g.наявність пільги;
+//h.показання приладів обліку;
+//i.сума оплати.
---
+++ DAL/RecieptInitialize.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class ReceiptInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<ReceiptContext>
+    {
+        protected override void Seed(ReceiptContext context)
+        {
+            var receipts = new List<Receipt>
+            {
+            new Receipt{ Adress = "Kharkov", Date = DateTime.Now, Name = "Lysak Kolyan", Privilege = true, ScoreNumber = 1,
+             ServiceName = "Remont", SnstrumentReadings = "A", SumPayment = 122}
+            };
+            receipts.ForEach(s => context.Receipts.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
